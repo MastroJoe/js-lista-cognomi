3fa8c75384fa1b4e98710f8e8c045c60ec29a12f
Giorgio <giorgio.pignattari@gmail.it>
2021-03-04 17:22:54 +0100

    finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,15 @@ var listaCognomi = ['Bianchi', 'Rossi', 'Perlari', 'Lodigiani', 'Mastrobattista'
 var cognome = prompt("Inserisci il tuo cognome:");
 
 // CONTROLLO
-// se la parola inserita ha la prima lettera maiuscola
-if (cognome.charAt(0) === cognome.charAt(0).toUpperCase()) {
+// se la parola inserita è un numero
+if (!isNaN(cognome)) {
+  // stampo messaggio di errore
+  console.log('Attenzione! Non hai inserito una parola.');
+  // altrimenti se la parola inserita ha la prima lettera maiuscola
+} else if (cognome.charAt(0) === cognome.charAt(0).toUpperCase()) {
   // inserisco il cognome dell'utente nella lista
   listaCognomi.push(cognome);
+  // altrimenti
 } else {
   // stampo messaggio di errore
   console.log('Attenzione! La prima lettera deve essere Maiuscola.');
@@ -20,3 +25,8 @@ listaCognomi.sort();
 
 // stampo e controllo la lista
 console.log(listaCognomi);
+
+// stampo la posizione del cognome appena inserito
+for (var i = 0; i < listaCognomi.length; i++) {
+  console.log(listaCognomi[i] + " = posizione n. " + (i + 1));
+}
